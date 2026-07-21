dffb722f7ef31059f16a8a8a5ae39bded41bafb4
malklera <malklerulo@gmail.com>
2026-07-21 14:46:41 -0300

    feat: right before testing processMovement

--- process.go
+++ process.go
@@ -1,133 +1,18 @@
 package main
 
 import (
+	// "context"
+	// "database/sql"
+	"fmt"
+	// "os"
+	// "path/filepath"
 	"strconv"
 	"strings"
+	// "time"
 
 	"cadetRevenue/internal/database"
+	uuid "github.com/gofrs/uuid/v5"
 )
-
-// processNote accept the name of a file, extract the data from it, return a
-// slice of struct `Entry`, returns at any error, the notes are supposed
-// to be formated
-// func processNote(nameNote string) ([]database.Entry, error) {
-// 	fmt.Println()
-// 	fmt.Println("Processing:", nameNote)
-//
-// 	orgNote := filepath.Join(formatedDir, nameNote)
-// 	data, err := os.ReadFile(orgNote)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("os.ReadFile(%s): %w", orgNote, err)
-// 	}
-// 	content := strings.Split(string(data), "\n")
-//
-// 	// TODO: when i change the name of the file, this has to be updated
-// 	// validate the name first
-// 	// Get the year
-// 	year := fileNameRe.FindStringSubmatch(nameNote)[2]
-//
-// 	canon := int64(0)
-//
-// 	entries := make([]database.Entry, 0, 6)
-// 	movements := make([]database.Movement, 0, 12)
-// 	n := 0
-// 	for n < len(content) {
-// 		var entry database.Entry
-// 		var movement database.Movement
-// 		entry.Canon = canon
-// 		date := year + "-"
-// 		switch {
-// 		case canonRe.MatchString(content[n]):
-// 			line := strings.Split(content[n], " ")
-//
-// 			canon, err = strconv.ParseInt(line[1], 10, 64)
-// 			if err != nil {
-// 				return nil, fmt.Errorf("canonRe.MatchString(%s): strconv.Atoi(%s): %w", content[n], line[1], err)
-// 			}
-// 			n++
-// 		case dayWorkRe.MatchString(content[n]):
-// 			// CONTINUE HERE
-// 			_, cDate, _ := strings.Cut(content[n], " ")
-// 			day, month, _ := strings.Cut(cDate, "/")
-// 			date += month + "-" + day
-// 			entry.Date, err = time.Parse(time.DateOnly, date)
-// 			if err != nil {
-// 				return nil, fmt.Errorf("dayWorkRe.MatchString(%s): time.Parse(%s, %s): %w", content[n], time.DateOnly, date, err)
-// 			}
-// 			n++
-// 			// here process the procedings and advance the counter
-// 			expensesM := 0
-// 			entry.IncomeM, expensesM, err = processProcedings(content[n])
-// 			if err != nil {
-// 				return nil, fmt.Errorf("dayWorkRe.MatchString(%s): processProcedings(%s): %w", content[n-1], content[n], err)
-// 			}
-// 			n++
-// 			expensesT := 0
-// 			entry.IncomeT, expensesT, err = processProcedings(content[n])
-// 			if err != nil {
-// 				return nil, fmt.Errorf("dayWorkRe.MatchString(%s): processProcedings(%s): %w", content[n-2], content[n], err)
-// 			}
-// 			n++
-// 			entry.Expenses = expensesM + expensesT
-// 			entries = append(entries, entry)
-// 		default:
-// 			return nil, fmt.Errorf("line '%s' of file '%s' has the wrong format", content[n], nameNote)
-// 		}
-// 	}
-// 	return entries, nil
-// }
-
-// processProcedings take in a valid string of `procedingsRe`, and extract its values
-func processProcedings(content string, entryID int64) ([]database.Movement, error) {
-	var procedings []database.Movement
-	line := strings.Split(content, ":")
-	shift := line[0]
-
-	// m:0
-	// t:2000
-	// t:2000+3000+2500
-	// m:-4500
-	// t:2000+3000+2500-3300
-	switch {
-	case strings.Contains(line[1], "-"):
-		hasExp := strings.Split(line[1], "-")
-		// NOTE: only allow for one -int, do not need to calculate len
-		// i do have to do it because of m:-4500
-		exp, err := strconv.ParseInt(hasExp[len(hasExp)-1], 10, 64)
-		if err != nil {
-			return nil, err
-		}
-		proc := database.Movement{EntryID: entryID, Shift: shift, Amount: exp * -1}
-		procedings = append(procedings, proc)
-		if len(hasExp) > 1 && hasExp[0] != "" {
-			for p := range strings.SplitSeq(hasExp[0], "+") {
-				amount, err := strconv.ParseInt(p, 10, 64)
-				if err != nil {
-					return nil, err
-				}
-				proc := database.Movement{EntryID: entryID, Shift: shift, Amount: amount}
-				procedings = append(procedings, proc)
-			}
-		}
-	case strings.Contains(line[1], "+"):
-		for p := range strings.SplitSeq(line[1], "+") {
-			amount, err := strconv.ParseInt(p, 10, 64)
-			if err != nil {
-				return nil, err
-			}
-			proc := database.Movement{EntryID: entryID, Shift: shift, Amount: amount}
-			procedings = append(procedings, proc)
-		}
-	default:
-		amount, err := strconv.ParseInt(line[1], 10, 64)
-		if err != nil {
-			return nil, err
-		}
-		proc := database.Movement{EntryID: entryID, Shift: shift, Amount: amount}
-		procedings = append(procedings, proc)
-	}
-	return procedings, nil
-}
 
 // processNotes process all notes in `formatedDir`, move the notes correctly
 // formated to `processedDir`.
@@ -137,6 +22,7 @@ func processProcedings(content string, entryID int64) ([]database.Movement, erro
 // 	if err != nil {
 // 		return fmt.Errorf("listFiles(%s): %w", path, err)
 // 	}
+//
 // 	ctx := context.Background()
 // 	db, err := sql.Open("sqlite3", "entries.db")
 // 	if err != nil {
@@ -144,8 +30,14 @@ func processProcedings(content string, entryID int64) ([]database.Movement, erro
 // 	}
 // 	defer db.Close()
 //
-// 	for n := range listNotes {
-// 		entries, err := processNote(listNotes[n])
+// 	for n, note := range listNotes {
+// 		orgNote := filepath.Join(formatedDir, note)
+// 		data, err := os.ReadFile(orgNote)
+// 		if err != nil {
+// 			return fmt.Errorf("os.ReadFile(%s): %w", orgNote, err)
+// 		}
+//
+// 		entries, err := processNote(note, data)
 // 		if err != nil {
 // 			fmt.Fprintf(os.Stderr, "error processing note '%s': %v\n", listNotes[n], err)
 // 			continue
@@ -168,4 +60,148 @@ func processProcedings(content string, entryID int64) ([]database.Movement, erro
 // 	return nil
 // }
 
+// processNote accept the name of a file, extract the data from it, return a
+// slice of struct `Entry`, returns at any error, the notes are supposed
+// to be formated
+// func processNote(nameNote string, data []byte) ([]database.Entry, error) {
+// 	fmt.Println()
+// 	fmt.Println("Processing:", nameNote)
+//
+// 	content := strings.Split(string(data), "\n")
+//
+// 	year := fileNameRe.FindStringSubmatch(nameNote)[1]
+//
+// 	canon := int64(0)
+//
+// 	entries := make([]database.Entry, 0, 6)
+// 	movements := make([]database.Movement, 0, 12)
+// 	n := 0
+// 	for n < len(content) {
+// 		entryU7, err := uuid.NewV7()
+// 		if err != nil {
+// 			return nil, fmt.Errorf("uuid.NewV7(): %w", err)
+// 		}
+// 		entry := database.Entry{ID: entryU7, Canon: canon}
+// 		movU7, err := uuid.NewV7()
+// 		if err != nil {
+// 			return nil, fmt.Errorf("uuid.NewV7(): %w", err)
+// 		}
+// 		movement := database.Movement{ID: movU7, EntryID: entry.ID}
+// 		date := year + "-"
+// 		switch {
+// 		case canonRe.MatchString(content[n]):
+// 			line := strings.Split(content[n], " ")
+//
+// 			c, err := strconv.ParseInt(line[1], 10, 64)
+// 			if err != nil {
+// 				return nil, fmt.Errorf("canonRe.MatchString(%s): strconv.ParseInt(%s, 10, 64): %w", content[n], line[1], err)
+// 			}
+// 			canon = c
+// 			n++
+// 		case dayWorkRe.MatchString(content[n]):
+// 			_, cDate, _ := strings.Cut(content[n], " ")
+// 			day, month, _ := strings.Cut(cDate, "/")
+// 			date += month + "-" + day
+// 			pDate, err := time.Parse(time.DateOnly, date)
+// 			if err != nil {
+// 				return nil, fmt.Errorf("dayWorkRe.MatchString(%s): time.Parse(%s, %s): %w", content[n], time.DateOnly, date, err)
+// 			}
+// 			entry.Date = pDate
+// 			// Read day-date and advance counter
+// 			n++
+// 			morning, expensesM, err := processMovement(content[n])
+// 			if err != nil {
+// 				return nil, fmt.Errorf("dayWorkRe.MatchString(%s): processProcedings(%s): %w", content[n-1], content[n], err)
+// 			}
+// 			n++
+// 			expensesT := 0
+// 			entry.IncomeT, expensesT, err = processMovement(content[n])
+// 			if err != nil {
+// 				return nil, fmt.Errorf("dayWorkRe.MatchString(%s): processProcedings(%s): %w", content[n-2], content[n], err)
+// 			}
+// 			n++
+// 			entry.Expenses = expensesM + expensesT
+// 			entries = append(entries, entry)
+// 		default:
+// 			return nil, fmt.Errorf("line '%s' of file '%s' has the wrong format", content[n], nameNote)
+// 		}
+// 	}
+// 	return entries, nil
+// }
 
+// processMovement take in a valid string of `morningRe` or `afternoonRe`,
+// and extract its values
+func processMovement(entryID uuid.UUID, content string) ([]database.Movement, error) {
+	var items []database.Movement
+	line := strings.Split(content, ":")
+	shift := line[0]
+
+	// m:0
+	// t:2000
+	// t:2000+3000+2500
+	// m:-4500
+	// t:2000+3000+2500-3300
+	switch {
+	case strings.Contains(line[1], "-"):
+		hasExp := strings.Split(line[1], "-")
+		// NOTE: only allow for one -int, do not need to calculate len
+		// i do have to do it because of m:-4500
+		exp, err := strconv.ParseInt(hasExp[len(hasExp)-1], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("strings.Contains(%s, \"-\"): strconv.ParseInt(%s, 10, 64): %w", line[1], hasExp[len(hasExp)-1], err)
+		}
+
+		procU7, err := uuid.NewV7()
+		if err != nil {
+			return nil, fmt.Errorf("uuid.NewV7(): %w", err)
+		}
+
+		proc := database.Movement{ID: procU7, EntryID: entryID, Shift: shift, Amount: exp * -1}
+		items = append(items, proc)
+		if len(hasExp) > 1 && hasExp[0] != "" {
+			for p := range strings.SplitSeq(hasExp[0], "+") {
+				amount, err := strconv.ParseInt(p, 10, 64)
+				if err != nil {
+					return nil, fmt.Errorf("strings.Contains(%s, \"-\"): strconv.ParseInt(%s, 10, 64): %w", line[1], p, err)
+				}
+
+				procU7, err := uuid.NewV7()
+				if err != nil {
+					return nil, fmt.Errorf("uuid.NewV7(): %w", err)
+				}
+
+				proc := database.Movement{ID: procU7, EntryID: entryID, Shift: shift, Amount: amount}
+				items = append(items, proc)
+			}
+		}
+	case strings.Contains(line[1], "+"):
+		for p := range strings.SplitSeq(line[1], "+") {
+			amount, err := strconv.ParseInt(p, 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("strings.Contains(%s, \"+\"): strconv.ParseInt(%s, 10, 64): %w", line[1], p, err)
+			}
+
+			procU7, err := uuid.NewV7()
+			if err != nil {
+				return nil, fmt.Errorf("uuid.NewV7(): %w", err)
+			}
+
+			proc := database.Movement{ID: procU7, EntryID: entryID, Shift: shift, Amount: amount}
+			items = append(items, proc)
+		}
+	default:
+		amount, err := strconv.ParseInt(line[1], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("default: strconv.ParseInt(%s, 10, 64): %w", line[1], err)
+		}
+
+		procU7, err := uuid.NewV7()
+		if err != nil {
+			return nil, fmt.Errorf("uuid.NewV7(): %w", err)
+		}
+
+		proc := database.Movement{ID: procU7, EntryID: entryID, Shift: shift, Amount: amount}
+		items = append(items, proc)
+	}
+	return items, nil
+}
--- process_test.go
+++ process_test.go
@@ -1,8 +1,11 @@
 package main
-//
-// import (
-// 	"testing"
-// )
+
+import (
+	"testing"
+
+	"cadetRevenue/internal/database"
+	uuid "github.com/gofrs/uuid/v5"
+)
 
 // TODO: fix this
 // func TestProcessProcedings(t *testing.T) {
@@ -32,3 +35,26 @@ package main
 // 		})
 // 	}
 // }
+
+func TestProcessMovement(t *testing.T) {
+	entryU7 := uuid.Must(uuid.FromString("019f8237-87e8-742b-9972-c08d896cb97c"))
+	movU7 := uuid.Must(uuid.FromString("019f8244-fd05-7cfe-b31e-21a6f73eeefb"))
+	var tests = []struct {
+		name    string
+		entryID uuid.UUID
+		content string
+		wantMov []database.Movement
+		wantErr error
+	}{
+		{"no sign", entryU7, "m:2000", []database.Movement{database.Movement{ID: movU7, EntryID: entryU7, Shift: "m", Amount: int64(2000)}}, nil},
+		{"one +", entryU7, "t:2000+2000", []database.Movement{database.Movement{ID: movU7, EntryID: entryU7, Shift: "m", Amount: int64(2000)}, database.Movement{ID: movU7, EntryID: entryU7, Shift: "m", Amount: int64(2000)}}, nil},
+		{"m:-4500", 0, -4500, nil},
+		{"M:2500+2200+2500+6000+2000+4000+4000+2000-14800", 25200, -14800, nil},
+		{"t:2000+5000+2000+2000+2000+3000+2500-3300", 18500, -3300, nil},
+		{"M:2000++2500+4500+4500+4000-2000", 0, 0, strconv.ErrSyntax},
+		{"T:-2000+3000", 0, 0, strconv.ErrSyntax}, // TODO: this will change when i update the regex
+		// {"T:-2000-3000", 0, 0, strconv.ErrSyntax}, // TODO: this will change when i update the regex
+		{"T:2000+", 0, 0, strconv.ErrSyntax}, // TODO: this will change when i update the regex
+
+	}
+}
