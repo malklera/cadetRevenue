c37dc4336976b2f724f00a022ef3d41b1ec115ec
malklera <malklerulo@gmail.com>
2026-07-21 15:21:08 -0300

    fix: now TestProcessMovement works

--- process_test.go
+++ process_test.go
@@ -3,58 +3,102 @@ package main
 import (
 	"testing"
 
-	"cadetRevenue/internal/database"
 	uuid "github.com/gofrs/uuid/v5"
 )
 
-// TODO: fix this
-// func TestProcessProcedings(t *testing.T) {
-// 	var contents = []struct {
-// 		content  string
-// 		income   int
-// 		expenses int
-// 		err      error
-// 	}{
-// 		{"M:2000", 2000, 0, nil},
-// 		{"T:2000+2000", 4000, 0, nil},
-// 		{"m:-4500", 0, -4500, nil},
-// 		{"M:2500+2200+2500+6000+2000+4000+4000+2000-14800", 25200, -14800, nil},
-// 		{"t:2000+5000+2000+2000+2000+3000+2500-3300", 18500, -3300, nil},
-// 		{"M:2000++2500+4500+4500+4000-2000", 0, 0, strconv.ErrSyntax},
-// 		{"T:-2000+3000", 0, 0, strconv.ErrSyntax}, // TODO: this will change when i update the regex
-// 		// {"T:-2000-3000", 0, 0, strconv.ErrSyntax}, // TODO: this will change when i update the regex
-// 		{"T:2000+", 0, 0, strconv.ErrSyntax}, // TODO: this will change when i update the regex
-// 	}
-//
-// 	for _, tt := range contents {
-// 		t.Run(tt.content, func(t *testing.T) {
-// 			in, ex, er := processProcedings(tt.content)
-// 			if in != tt.income || ex != tt.expenses || !errors.Is(er, tt.err) {
-// 				t.Errorf("got '%d, %d, %v'; want '%d, %d, %v'", in, ex, er, tt.income, tt.expenses, tt.err)
-// 			}
-// 		})
-// 	}
-// }
-
 func TestProcessMovement(t *testing.T) {
 	entryU7 := uuid.Must(uuid.FromString("019f8237-87e8-742b-9972-c08d896cb97c"))
-	movU7 := uuid.Must(uuid.FromString("019f8244-fd05-7cfe-b31e-21a6f73eeefb"))
-	var tests = []struct {
-		name    string
-		entryID uuid.UUID
-		content string
-		wantMov []database.Movement
-		wantErr error
-	}{
-		{"no sign", entryU7, "m:2000", []database.Movement{database.Movement{ID: movU7, EntryID: entryU7, Shift: "m", Amount: int64(2000)}}, nil},
-		{"one +", entryU7, "t:2000+2000", []database.Movement{database.Movement{ID: movU7, EntryID: entryU7, Shift: "m", Amount: int64(2000)}, database.Movement{ID: movU7, EntryID: entryU7, Shift: "m", Amount: int64(2000)}}, nil},
-		{"m:-4500", 0, -4500, nil},
-		{"M:2500+2200+2500+6000+2000+4000+4000+2000-14800", 25200, -14800, nil},
-		{"t:2000+5000+2000+2000+2000+3000+2500-3300", 18500, -3300, nil},
-		{"M:2000++2500+4500+4500+4000-2000", 0, 0, strconv.ErrSyntax},
-		{"T:-2000+3000", 0, 0, strconv.ErrSyntax}, // TODO: this will change when i update the regex
-		// {"T:-2000-3000", 0, 0, strconv.ErrSyntax}, // TODO: this will change when i update the regex
-		{"T:2000+", 0, 0, strconv.ErrSyntax}, // TODO: this will change when i update the regex
 
+	var tests = []struct {
+		name       string
+		entryID    uuid.UUID
+		content    string
+		wantCount  int
+		wantAmount []int64
+		wantShift  string
+		wantErr    bool
+	}{
+		{
+			name:       "single value",
+			entryID:    entryU7,
+			content:    "m:2000",
+			wantCount:  1,
+			wantAmount: []int64{2000},
+			wantShift:  "m",
+		},
+		{
+			name:       "plus signs",
+			entryID:    entryU7,
+			content:    "t:2000+3000+2500",
+			wantCount:  3,
+			wantAmount: []int64{2000, 3000, 2500},
+			wantShift:  "t",
+		},
+		{
+			name:       "expense only",
+			entryID:    entryU7,
+			content:    "m:-4500",
+			wantCount:  1,
+			wantAmount: []int64{-4500},
+			wantShift:  "m",
+		},
+		{
+			name:       "income and expense",
+			entryID:    entryU7,
+			content:    "t:2000+3000-1500",
+			wantCount:  3,
+			wantAmount: []int64{-1500, 2000, 3000},
+			wantShift:  "t",
+		},
+		{
+			name:    "trailing plus errors",
+			entryID: entryU7,
+			content: "t:2000+",
+			wantErr: true,
+		},
+		{
+			name:    "double plus errors",
+			entryID: entryU7,
+			content: "m:2000++2500",
+			wantErr: true,
+		},
+		{
+			name:    "expenses not last item",
+			entryID: entryU7,
+			content: "t:-200+300",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := processMovement(tt.entryID, tt.content)
+			if tt.wantErr {
+				if err == nil {
+					t.Fatalf("expected error, got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if len(got) != len(tt.wantAmount) {
+				t.Fatalf("got %d movements, want %d", len(got), len(tt.wantAmount))
+			}
+			for i, mov := range got {
+				if mov.Amount != tt.wantAmount[i] {
+					t.Errorf("movement[%d].Amount = %d, want %d", i, mov.Amount, tt.wantAmount[i])
+				}
+				if mov.EntryID != tt.entryID {
+					t.Errorf("movement[%d].EntryID = %v, want %v", i, mov.EntryID, tt.entryID)
+				}
+				if mov.Shift != tt.wantShift {
+					t.Errorf("movement[%d].Shift = %q, want %q", i, mov.Shift, tt.wantShift)
+				}
+				if mov.ID == uuid.Nil {
+					t.Errorf("movement[%d].ID is nil UUID", i)
+				}
+			}
+		})
 	}
 }
